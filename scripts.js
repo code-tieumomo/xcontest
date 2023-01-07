80cc5a2cc5053c4f421a5b520ff1339bda7c0aff
Deadline Slayer <code.tieumomo@gmail.com>
2023-01-07 10:36:11 +0000

    Update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-AOS.init();
+AOS.init(
+    {
+        duration: 700,
+    }
+);
+
+$('.activities').slick({
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
